c946a6439fbe0932cfea6921ff0cb52c52169e69
bhanu1206 <116783487+bhanu1206@users.noreply.github.com>
2024-04-09 13:29:17 +0530

    Update llmCall.py

--- llmCall.py
+++ llmCall.py
@@ -14,7 +14,7 @@ def llm_call(code_body, module_path, language, import_module):
         language (str): The programming language of the code ('python' or 'java').
     """
     # Set your OpenAI API key
-    api_key = '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'
+    api_key = os.getenv('API_KEY')'
 
     runTestPython = """
         def suite():
